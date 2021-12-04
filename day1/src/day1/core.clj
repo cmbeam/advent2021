841647dc9285e7912ce8e6c6c2361c293b0c0768
Chris Beam <chris_beam_16@yahoo.com>
2021-12-03 22:34:16 -0500

    Day 1 part 2 in clojure

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -15,18 +15,31 @@
   (def inputs (list "0"))
   (with-open [rdr (clojure.java.io/reader file)]
     (doseq [line (line-seq rdr)]
-
       ;(println line)
       ;(println (first inputs))
       (if (> (Integer/parseInt line) (Integer/parseInt (first inputs)))
         (def increaseCount (+ increaseCount 1)) )
       (def inputs(cons line inputs ) )
       )
-
     )
   (int (dec increaseCount))
   )
 
+(defn increasesWithWindow [file]
+  (def increaseCount 0)
+  (def inputs (list "0" "0" "0"))
+  (with-open [rdr (clojure.java.io/reader file)]
+    (doseq [line (line-seq rdr)]
+      (if (> (+ (Integer/parseInt line) (Integer/parseInt (nth inputs 0)) (Integer/parseInt (nth inputs 1))) (+ (Integer/parseInt (nth inputs 0)) (Integer/parseInt (nth inputs 1)) (Integer/parseInt (nth inputs 2))))
+        (def increaseCount (+ increaseCount 1)))
+      (def inputs(cons line inputs ) )
+      )
+    )
+  (int (- increaseCount 3))
+  )
 
-(def coordinates (increases "../../resources/input_day1.txt"))
-(println coordinates)
+
+(def increases (increases "../../resources/input_day1.txt"))
+(println "Part 1: " increases)
+(def increases (increasesWithWindow "../../resources/input_day1.txt"))
+(println "Part 2: " increases)
