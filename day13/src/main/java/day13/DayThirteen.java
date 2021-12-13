0ff3386952c40884f7cf922ecf7497f304b11404
Chris Beam <chris_beam_16@yahoo.com>
2021-12-13 08:32:53 -0500

    Day 11 complete

---
+++ day13/src/main/java/day13/DayThirteen.java
@@ -0,0 +1,20 @@
+package day13;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DayThirteen {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("/Users/cbeam/GIT/advent2021/day13/src/main/resources/input_day13.txt");
+            Scanner myReader = new Scanner(myObj);
+
+            while (myReader.hasNextLine()) {
+                String[] nodeInputs = myReader.nextLine().split("-");
+            }
+        }
+        catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
