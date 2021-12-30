33ba6756c932a0b67c9e06435fb56a97811c0109
Chris Beam <chris_beam_16@yahoo.com>
2021-12-29 21:00:39 -0500

    Day 24 complete

--- day24/src/main/java/day24/DayTwentyFour.java
+++ day24/src/main/java/day24/DayTwentyFour.java
@@ -17,105 +17,53 @@ public class DayTwentyFour {
         }
 
 
-//        Map<String,Integer> variables = new HashMap<>();
-//        variables.put("w",0);
-//        variables.put("x",0);
-//        variables.put("y",0);
-//        variables.put("z",0);
-//
-//        for (String instruction:inputs
-//             ) {
-//            System.out.println(instruction);
-//            String[] parts = instruction.split("\\s");
-//            int bInt;
-//            switch (parts[0]){
-//                case "inp":
-//                    try {
-//                        Scanner sc= new Scanner(System.in);
-//                        System.out.print("Input: ");
-//                        int userInput = sc.nextInt();
-//
-//                        variables.put(parts[1],userInput);
-//                    }
-//                    catch (Exception e){
-//                        System.out.println("read exception: "+e.getMessage());
-//                    }
-//                    break;
-//                case "mul":
-//                    if(parts[2].matches("-?\\d+")) {
-//                        bInt = Integer.parseInt(parts[2]);
-//                    }
-//                    else{
-//                       bInt = variables.get(parts[2]);
-//                    }
-//                    variables.put(parts[1],variables.get(parts[1]) * bInt);
-//                    break;
-//                case "add":
-//                    if(parts[2].matches("-?\\d+")) {
-//                        bInt = Integer.parseInt(parts[2]);
-//                    }
-//                    else{
-//                        bInt = variables.get(parts[2]);
-//                    }
-//                    variables.put(parts[1],variables.get(parts[1]) + bInt);
-//                    break;
-//                case "div":
-//                    if(parts[2].matches("-?\\d+")) {
-//                        bInt = Integer.parseInt(parts[2]);
-//                    }
-//                    else{
-//                        bInt = variables.get(parts[2]);
-//                    }
-//                    variables.put(parts[1],variables.get(parts[1]) / bInt);
-//                    break;
-//                case "mod":
-//                    if(parts[2].matches("-?\\d+")) {
-//                        bInt = Integer.parseInt(parts[2]);
-//                    }
-//                    else{
-//                        bInt = variables.get(parts[2]);
-//                    }
-//                    variables.put(parts[1], Math.floorMod(variables.get(parts[1]), bInt));
-//                    break;
-//                case "eql":
-//                    if(parts[2].matches("-?\\d+")) {
-//                        bInt = Integer.parseInt(parts[2]);
-//                    }
-//                    else{
-//                        bInt = variables.get(parts[2]);
-//                    }
-//                    if(variables.get(parts[1]) == bInt){
-//                        variables.put(parts[1], 1);
-//                    }
-//                    else {
-//                        variables.put(parts[1], 0);
-//                    }
-//                    break;
-//                default:
-//                    break;
-//            }
-//
-//        }
-//        System.out.println(variables.toString());
 
+        long highest = 0;
+        long lowest = 99999999999999L;
+        for (int i = 9; i < 10 ; i++) {
+            for (int j = 1; j < 7; j++) {
+                for (int k = 3; k < 10 ; k++) {
+                    for (int l = 1; l < 9 ; l++) {
+                        for (int m = 1; m < 6 ; m++) {
+                            for (int n = 1; n < 5 ; n++) {
+                                for (int o = 1; o < 3; o++) {
+                                    String w1 = ""+(o+7);
+                                    String w2 = ""+(j+3);
+                                    String w3 = ""+(i-8);
+                                    String w4 = "" + i;
+                                    String w5 = "" + j;
+                                    String w6 = ""+(m+4);
+                                    String w7 = ""+(l+1);
+                                    String w8 = ""+(k-2);
+                                    String w9 = "" + k;
+                                    String w10 = "" + l;
+                                    String w11 = "" + m;
+                                    String w12 = ""+(n+5);
+                                    String w13 = "" + n;
+                                    String w14 = "" + o;
+                                    long calc = Long.parseLong(w1+w2+w3+w4+w5+w6+w7+w8+w9+w10+w11+w12+w13+w14);
+                                    if(calc > highest)
+                                        highest = calc;
+                                    if(calc < lowest)
+                                        lowest = calc;
 
-        String testSerial = "13579246899999";
-        //String testSerial = "99999999999999";
-       runMonad(testSerial,inputs);
+                                }
 
-//        for (int i = 9999999; i >= 1111111 ; i--) {
-//            for (int j = 9999999; j >= 1111111; j--) {
-//                String serial = ""+i+""+j;
-//                //System.out.print(serial+": ");
-//                if(!serial.contains("0")){
-//                    //System.out.println("run");
-//                    if(runMonad(serial,inputs))
-//                        break;
-//                }
-//            }
-//        }
+                            }
 
+                        }
 
+                    }
+
+                }
+
+            }
+
+        }
+        System.out.println("Highest: " +highest);
+        runMonad(""+highest,inputs);
+        System.out.println("Lowest: " +lowest);
+        runMonad(""+lowest,inputs);
     }
 
     private static boolean runMonad(String serial, List<String> code){
@@ -130,7 +78,7 @@ public class DayTwentyFour {
 
         for (String instruction:code
         ) {
-           System.out.println(instruction);
+           //System.out.println(instruction);
             String[] parts = instruction.split("\\s");
             long bInt;
             switch (parts[0]){
@@ -143,7 +91,9 @@ public class DayTwentyFour {
                     catch (Exception e){
                         System.out.println("read exception: "+e.getMessage());
                     }
-                    System.out.println();
+                    System.out.println("Inp");
+                    System.out.println(variables.toString());
+
                     break;
                 case "mul":
                     if(parts[2].matches("-?\\d+")) {
@@ -198,7 +148,7 @@ public class DayTwentyFour {
                 default:
                     break;
             }
-            System.out.println(variables.toString());
+            //System.out.println(variables.toString());
 
         }
         if(variables.get("z") == 0) {
